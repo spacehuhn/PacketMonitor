482107b323f56dd6c48a509eab46d63c42337648
Stefan Kremser <stef.1497@gmail.com>
2017-06-06 12:49:08 +0200

    Added comments and copyright notice

--- esp8266_packet_monitor/esp8266_packet_monitor.ino
+++ esp8266_packet_monitor/esp8266_packet_monitor.ino
@@ -6,23 +6,25 @@ extern "C" {
   #include "user_interface.h"
 }
 
+//===== SETTINGS =====//
 //include the library you need
 #include "SSD1306.h"
 //#include "SH1106.h"
 
-#define btn 0
+#define btn 0 /* GPIO 0 = FLASH BUTTON */
 
 //create display(Adr, SDA-pin, SCL-pin)
 SSD1306 display(0x3c, 5, 4); //GPIO 5 = D1, GPIO 4 = D2
 //SH1106 display(0x3c, 5, 4);
   
-//===== SETTINGS =====//
 #define maxCh 13 //max Channel -> US = 11, EU = 13, Japan = 14
 #define ledPin 2 //led pin ( 2 = built-in LED)
 #define packetRate 5 //min. packets before it gets recognized as an attack
 
 #define flipDisplay true
 
+
+//===== Run-Time variables =====//
 unsigned long prevTime = 0;
 unsigned long curTime = 0;
 unsigned long pkts = 0;
@@ -41,9 +43,36 @@ void sniffer(uint8_t *buf, uint16_t len) {
   }
 }
 
+void getMultiplicator(){
+  maxVal = 1;
+  for(int i=0;i<128;i++){
+    if(val[i] > maxVal) maxVal = val[i];
+  }
+  if(maxVal > 53) multiplicator = (double)53/(double)maxVal;
+  else multiplicator = 1;
+}
+
+//===== SETUP =====//
 void setup() {
+  /* start Display */
+  display.init();
+  if(flipDisplay) display.flipScreenVertically();
+
+  /* show start screen */
+  display.clear();
+  display.setFont(ArialMT_Plain_16);
+  display.drawString(0, 0, "Packet-");
+  display.drawString(0, 16, "Monitor");
+  display.setFont(ArialMT_Plain_10);
+  display.drawString(0, 40, "Copyright (c) 2017");
+  display.drawString(0, 50, "Stefan Kremser");
+  display.display();
+  delay(2500);
+
+  /* start Serial */
   Serial.begin(115200);
 
+  /* load last saved channel */
   EEPROM.begin(4096);
   curChannel = EEPROM.read(2000);
   if(curChannel < 1 || curChannel > maxCh){
@@ -51,13 +80,12 @@ void setup() {
     EEPROM.write(2000, curChannel);
     EEPROM.commit();
   }
-  
-  display.init();
-  if(flipDisplay) display.flipScreenVertically();
-  pinMode(btn, INPUT);
 
+  /* set pin modes for button and LED */
+  pinMode(btn, INPUT);
   pinMode(ledPin, OUTPUT);
 
+  /* setup wifi */
   wifi_set_opmode(STATION_MODE);
   wifi_promiscuous_enable(0);
   WiFi.disconnect();
@@ -68,57 +96,59 @@ void setup() {
   Serial.println("starting!");
 }
 
-void getMultiplicator(){
-  maxVal = 1;
-  for(int i=0;i<128;i++){
-    if(val[i] > maxVal) maxVal = val[i];
-  }
-  if(maxVal > 53) multiplicator = (double)53/(double)maxVal;
-  else multiplicator = 1;
-}
-
+//===== LOOP =====//
 void loop() {
   curTime = millis();
-  
+
+  //on button release
   if(digitalRead(btn) == LOW){
     if(canBtnPress) canBtnPress = false;
   }else if(!canBtnPress){
     canBtnPress = true;
-    
+
+    //switch channel
     curChannel++;
     if(curChannel > maxCh) curChannel = 1;
     wifi_set_channel(curChannel);
     for(int i=0;i<128;i++) val[i] = 0;
     pkts = 0;
     multiplicator = 1;
-     
+
+    //save changes
     EEPROM.write(2000, curChannel);
     EEPROM.commit();
 
+    //draw display
     display.clear();
     for(int i=0;i<128;i++) display.drawLine(i, 64, i, 64-val[i]*multiplicator);
     display.drawString(0, 0, " Ch: " + (String)curChannel + " Pkts: "+(String)pkts);
     display.display();
   }
 
+  //every second
   if(curTime - prevTime >= 1000){
     prevTime = curTime;
 
+    //move every packet bar one pixel to the left
     for(int i=0;i<127;i++){
       val[i] = val[i+1];
     }
     val[127] = pkts;
 
+    //recalculate scaling factor
     getMultiplicator();
 
+    //deauth alarm
     if(deauths > packetRate) digitalWrite(ledPin, LOW);
     else digitalWrite(ledPin, HIGH);
 
+    //draw display
     display.clear();
     for(int i=0;i<128;i++) display.drawLine(i, 64, i, 64-val[i]*multiplicator);
     display.drawString(0, 0, " Ch: " + (String)curChannel + " Pkts: "+(String)pkts);
     display.display();
-    
+
+    //reset counters
     deauths = 0;
     pkts = 0;
   }
